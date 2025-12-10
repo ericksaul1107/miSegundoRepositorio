e0109d0cad11db48a10ee7eb0398cf532dec2bc2
ericksaul1107 <erickrosalesdelamora@MacBook-Air-de-Erick.local>
2025-12-10 03:38:05 -0600

    Commit de modificacion

--- untitled/src/Clase01.java
+++ untitled/src/Clase01.java
@@ -1,5 +1,9 @@
 public class Clase01 {
     public static void main(String[] args) {
         System.out.println("MiPrimera clase");
+        metodo();
+    }
+    public static void metodo(){
+        System.out.println("Soy un metodo");
     }
 }
